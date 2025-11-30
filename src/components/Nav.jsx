61b69f2221a313e04a83d2a97906fcca004d5cdd
Vertrowd <rudrapratapsinghind42002@gmail.com>
2025-11-30 14:35:54 +0530

    my first react project

---
+++ src/components/Nav.jsx
@@ -0,0 +1,20 @@
+const Nav = () =>{
+    return (
+        <nav className="container">
+      <div className="logo">
+        <img src="public\images\image.png" className="image" alt="" />
+        
+      </div>
+      <ul>
+        <li href="#">Menu</li>
+        <li href="#">Location</li>
+        <li href="#">About</li>
+        <li href="#">Contact</li>
+        </ul>
+      <button className="login-btn">Login</button>
+    </nav>
+
+    );
+}
+
+export default Nav
